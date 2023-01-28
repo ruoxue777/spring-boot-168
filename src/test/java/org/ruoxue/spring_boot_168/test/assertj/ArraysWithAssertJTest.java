0df1d5671e078bb52357fb10cad0612201a085d1
ruoxue <ruoxue777@gmail.com>
2023-01-28 13:10:27 +0800

    add ArraysWithAssertJTest

---
+++ src/test/java/org/ruoxue/spring_boot_168/test/assertj/ArraysWithAssertJTest.java
@@ -0,0 +1,119 @@
+package org.ruoxue.spring_boot_168.test.assertj;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+public class ArraysWithAssertJTest {
+
+	public ArraysWithAssertJTest() {
+
+	}
+
+	@NoArgsConstructor
+	@Getter
+	@Setter
+	@Builder
+	public static class Drink {
+		private String name;
+		private double quantity;
+		private int type;
+
+		public Drink(String name, double quantity, int type) {
+			this.name = name;
+			this.quantity = quantity;
+			this.type = type;
+		}
+	}
+
+	@Test
+	public void extracting() {
+		Drink coffee = new Drink("coffee", 4, 2);
+		Drink juice = new Drink("juice", 5, 2);
+		Drink tea = new Drink("tea", 6, 2);
+		Drink[] array = new Drink[] { coffee, juice, tea };
+
+		assertThat(array).extracting(Drink::getName).containsExactly("coffee", "juice", "tea");
+	}
+
+	@Test
+	public void extractingTuple() {
+		Drink coffee = new Drink("coffee", 4, 2);
+		Drink juice = new Drink("juice", 5, 2);
+		Drink tea = new Drink("tea", 6, 2);
+		Drink[] array = new Drink[] { coffee, juice, tea };
+
+		assertThat(array).extracting("name", "quantity").containsExactly(tuple("coffee", 4d), tuple("juice", 5d),
+				tuple("tea", 6d));
+
+		assertThat(array).extracting(e -> e.getName(), Drink::getQuantity).containsExactly(tuple("coffee", 4d),
+				tuple("juice", 5d), tuple("tea", 6d));
+	}
+
+	@Test
+	public void allMatch() {
+		Drink coffee = new Drink("coffee", 4, 2);
+		Drink juice = new Drink("juice", 5, 2);
+		Drink tea = new Drink("tea", 6, 2);
+		Drink[] array = new Drink[] { coffee, juice, tea };
+
+		assertThat(array).allMatch(e -> e.getType() == 2).doesNotContainNull();
+	}
+
+	@Test
+	public void anyMatch() {
+		Drink coffee = new Drink("coffee", 4, 2);
+		Drink juice = new Drink("juice", 5, 2);
+		Drink tea = new Drink("tea", 6, 2);
+		Drink[] array = new Drink[] { coffee, juice, tea };
+
+		assertThat(array).anyMatch(e -> e.getName().equals("coffee")).doesNotContainNull();
+	}
+
+	@Test
+	public void noneMatch() {
+		Drink coffee = new Drink("coffee", 4, 2);
+		Drink juice = new Drink("juice", 5, 2);
+		Drink tea = new Drink("tea", 6, 2);
+		Drink[] array = new Drink[] { coffee, juice, tea };
+
+		assertThat(array).noneMatch(e -> e.getType() == 0).doesNotContainNull();
+	}
+
+	@Test
+	public void filteredOn() {
+		int expectedSize = 3;
+		Drink coffee = new Drink("coffee", 4, 2);
+		Drink juice = new Drink("juice", 5, 2);
+		Drink tea = new Drink("tea", 6, 2);
+		Drink[] array = new Drink[] { coffee, juice, tea };
+
+		assertThat(array).filteredOn(Drink::getType, 2).containsOnly(coffee, juice, tea).hasSize(expectedSize);
+	}
+
+	@Test
+	public void anySatisfy() {
+		Drink coffee = new Drink("coffee", 4, 2);
+		Drink juice = new Drink("juice", 5, 2);
+		Drink tea = new Drink("tea", 6, 2);
+		Drink[] array = new Drink[] { coffee, juice, tea };
+
+		assertThat(array).allSatisfy(e -> {
+			assertThat(e.getType()).isEqualTo(2);
+			assertThat(e.getName()).isNotNull();
+		});
+
+		assertThat(array).anySatisfy(e -> {
+			assertThat(e.getType()).isEqualTo(2);
+			assertThat(e.getName()).isEqualTo("juice");
+		});
+
+		assertThat(array).noneSatisfy(e -> assertThat(e.getType()).isEqualTo(0));
+	}
+
+}
--- src/test/java/org/ruoxue/spring_boot_168/test/assertj/AssertJArrayTest.java
+++ src/test/java/org/ruoxue/spring_boot_168/test/assertj/AssertJArrayTest.java
@@ -3,6 +3,7 @@ package org.ruoxue.spring_boot_168.test.assertj;
 import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import org.ruoxue.spring_boot_168.test.assertj.ArraysWithAssertJTest.Drink;
 
 import lombok.Builder;
 import lombok.Getter;
@@ -79,72 +80,6 @@ public class AssertJArrayTest {
 		String[] array = new String[] { "coffee", "juice", "tea" };
 
 		assertThat(array).contains("coffee", atIndex(0)).contains("juice", atIndex(1)).contains("tea", atIndex(2));
-	}
-
-	@Test
-	public void extracting() {
-		Drink coffee = new Drink("coffee", 4, 2);
-		Drink juice = new Drink("juice", 5, 2);
-		Drink tea = new Drink("tea", 6, 2);
-		Drink[] array = new Drink[] { coffee, juice, tea };
-
-		assertThat(array).extracting(Drink::getName).containsExactly("coffee", "juice", "tea");
-	}
-
-	@Test
-	public void extractingTuple() {
-		Drink coffee = new Drink("coffee", 4, 2);
-		Drink juice = new Drink("juice", 5, 2);
-		Drink tea = new Drink("tea", 6, 2);
-		Drink[] array = new Drink[] { coffee, juice, tea };
-
-		assertThat(array).extracting("name", "quantity").containsExactly(tuple("coffee", 4d), tuple("juice", 5d),
-				tuple("tea", 6d));
-
-		assertThat(array).extracting(e -> e.getName(), Drink::getQuantity).containsExactly(tuple("coffee", 4d),
-				tuple("juice", 5d), tuple("tea", 6d));
-	}
-
-	@Test
-	public void anyMatch() {
-		Drink coffee = new Drink("coffee", 4, 2);
-		Drink juice = new Drink("juice", 5, 2);
-		Drink tea = new Drink("tea", 6, 2);
-		Drink[] array = new Drink[] { coffee, juice, tea };
-
-		assertThat(array).allMatch(e -> e.getType() == 2).anyMatch(e -> e.getName().equals("coffee"))
-				.noneMatch(e -> e.getType() == 0).doesNotContainNull();
-	}
-
-	@Test
-	public void filteredOn() {
-		int expectedSize = 3;
-		Drink coffee = new Drink("coffee", 4, 2);
-		Drink juice = new Drink("juice", 5, 2);
-		Drink tea = new Drink("tea", 6, 2);
-		Drink[] array = new Drink[] { coffee, juice, tea };
-
-		assertThat(array).filteredOn(Drink::getType, 2).containsOnly(coffee, juice, tea).hasSize(expectedSize);
-	}
-
-	@Test
-	public void anySatisfy() {
-		Drink coffee = new Drink("coffee", 4, 2);
-		Drink juice = new Drink("juice", 5, 2);
-		Drink tea = new Drink("tea", 6, 2);
-		Drink[] array = new Drink[] { coffee, juice, tea };
-
-		assertThat(array).allSatisfy(e -> {
-			assertThat(e.getType()).isEqualTo(2);
-			assertThat(e.getName()).isNotNull();
-		});
-
-		assertThat(array).anySatisfy(e -> {
-			assertThat(e.getType()).isEqualTo(2);
-			assertThat(e.getName()).isEqualTo("juice");
-		});
-
-		assertThat(array).noneSatisfy(e -> assertThat(e.getType()).isEqualTo(0));
 	}
 
 	@Test
