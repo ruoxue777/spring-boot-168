4f30a3f0b957499abf3ee69a8ce2c92c734df0c6
ruoxue <ruoxue777@gmail.com>
2023-02-17 10:04:45 +0800

    add NamedThreadFactory

--- src/main/java/org/ruoxue/spring_boot_168/config/ThreadPoolConfig.java
+++ src/main/java/org/ruoxue/spring_boot_168/config/ThreadPoolConfig.java
@@ -2,20 +2,21 @@ package org.ruoxue.spring_boot_168.config;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.ThreadPoolExecutor.CallerRunsPolicy;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.TimeUnit;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import io.micrometer.core.instrument.util.NamedThreadFactory;
 import lombok.extern.slf4j.Slf4j;
 
 @Configuration
 @Slf4j
 public class ThreadPoolConfig {
-	public static final int CORE_POOL_SIZE = Runtime.getRuntime().availableProcessors(); 
+	public static final int CORE_POOL_SIZE = Runtime.getRuntime().availableProcessors();
 	public static final int MAX_POOLSIZE = 50 * CORE_POOL_SIZE;
 	public static final long KEEP_LIVE_TIME = 60000L;
 	public static final int QUEUE_CAPACITY = 10;
@@ -63,5 +64,22 @@ public class ThreadPoolConfig {
 		);
 		log.info("prefix: " + prefix);
 		return executor;
+	}
+
+	public class NamedThreadFactory implements ThreadFactory {
+		private final AtomicInteger count = new AtomicInteger();
+		private final String prefix;
+
+		public NamedThreadFactory(String prefix) {
+			this.prefix = prefix;
+		}
+
+		@Override
+		public Thread newThread(Runnable r) {
+			Thread thread = new Thread(r);
+			int c = count.incrementAndGet();
+			thread.setName(prefix + "-" + c);
+			return thread;
+		}
 	}
 }
