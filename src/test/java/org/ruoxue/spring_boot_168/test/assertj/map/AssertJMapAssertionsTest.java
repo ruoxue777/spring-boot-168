f5f1eac6f09435dba3b0e0c2d7d5ee2d72f692d3
ruoxue <ruoxue777@gmail.com>
2023-08-17 18:01:48 +0800

    update test

--- src/test/java/org/ruoxue/spring_boot_168/test/assertj/map/AssertJMapAssertionsTest.java
+++ src/test/java/org/ruoxue/spring_boot_168/test/assertj/map/AssertJMapAssertionsTest.java
@@ -6,56 +6,9 @@ import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-import org.apache.commons.lang3.builder.ToStringBuilder;
-import org.apache.commons.lang3.builder.ToStringStyle;
 import org.junit.jupiter.api.Test;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
 public class AssertJMapAssertionsTest {
-
-	public AssertJMapAssertionsTest() {
-
-	}
-
-	@NoArgsConstructor
-	@Getter
-	@Setter
-	@Builder
-	public static class Fruit {
-		private String name;
-		private double quantity;
-		private int type;
-
-		public Fruit(String name, double quantity, int type) {
-			this.name = name;
-			this.quantity = quantity;
-			this.type = type;
-		}
-
-		public String toString() {
-			ToStringBuilder builder = new ToStringBuilder(this, ToStringStyle.JSON_STYLE);
-			builder.appendSuper(super.toString());
-			builder.append("name", name);
-			builder.append("quantity", quantity);
-			builder.append("type", type);
-			return builder.toString();
-		}
-	}
-	
-	@Test
-	public void hasSize() {
-		int expectedSize = 3;
-		Map<String, Integer> map = new HashMap<String, Integer>();
-		map.put("Grape", 18);
-		map.put("Kiwifruit", 19);
-		map.put("Lemon", 20);
-		System.out.println(map);
-		assertThat(map).hasSize(expectedSize);
-	}
 
 	@Test
 	public void containsKeys() {
@@ -65,6 +18,13 @@ public class AssertJMapAssertionsTest {
 		map.put("Lemon", 20);
 		System.out.println(map);
 		assertThat(map).containsKeys("Grape", "Kiwifruit");
+
+		Map<Integer, Integer> intMap = new HashMap<Integer, Integer>();
+		intMap.put(1, 18);
+		intMap.put(2, 19);
+		intMap.put(3, 20);
+		System.out.println(intMap);
+		assertThat(intMap).containsKeys(1, 2);
 	}
 
 	@Test
@@ -75,6 +35,13 @@ public class AssertJMapAssertionsTest {
 		map.put("Lemon", 20);
 		System.out.println(map);
 		assertThat(map).doesNotContainKeys("Papaya", "Strawberry");
+
+		Map<Integer, Integer> intMap = new HashMap<Integer, Integer>();
+		intMap.put(1, 18);
+		intMap.put(2, 19);
+		intMap.put(3, 20);
+		System.out.println(intMap);
+		assertThat(intMap).doesNotContainKeys(8, 9);
 	}
 
 	@Test
@@ -85,6 +52,13 @@ public class AssertJMapAssertionsTest {
 		map.put("Lemon", 20);
 		System.out.println(map);
 		assertThat(map).containsValues(18, 19);
+
+		Map<Integer, Integer> intMap = new HashMap<Integer, Integer>();
+		intMap.put(1, 18);
+		intMap.put(2, 19);
+		intMap.put(3, 20);
+		System.out.println(intMap);
+		assertThat(intMap).containsValues(18, 19);
 	}
 
 	@Test
@@ -95,6 +69,13 @@ public class AssertJMapAssertionsTest {
 		map.put("Lemon", 20);
 		System.out.println(map);
 		assertThat(map).doesNotContainValue(30);
+
+		Map<Integer, Integer> intMap = new HashMap<Integer, Integer>();
+		intMap.put(1, 18);
+		intMap.put(2, 19);
+		intMap.put(3, 20);
+		System.out.println(intMap);
+		assertThat(intMap).doesNotContainValue(99);
 	}
 
 	@Test
@@ -105,6 +86,13 @@ public class AssertJMapAssertionsTest {
 		map.put("Lemon", 20);
 		System.out.println(map);
 		assertThat(map).containsEntry("Grape", 18);
+
+		Map<Integer, Integer> intMap = new HashMap<Integer, Integer>();
+		intMap.put(1, 18);
+		intMap.put(2, 19);
+		intMap.put(3, 20);
+		System.out.println(intMap);
+		assertThat(intMap).containsEntry(1, 18);
 	}
 
 	@Test
@@ -115,6 +103,13 @@ public class AssertJMapAssertionsTest {
 		map.put("Lemon", 20);
 		System.out.println(map);
 		assertThat(map).doesNotContainEntry("Papaya", 28);
+
+		Map<Integer, Integer> intMap = new HashMap<Integer, Integer>();
+		intMap.put(1, 18);
+		intMap.put(2, 19);
+		intMap.put(3, 20);
+		System.out.println(intMap);
+		assertThat(intMap).doesNotContainEntry(9, 99);
 	}
 
 	@Test
@@ -125,6 +120,13 @@ public class AssertJMapAssertionsTest {
 		map.put("Lemon", 20);
 		System.out.println(map);
 		assertThat(map).containsOnly(entry("Kiwifruit", 19), entry("Lemon", 20), entry("Grape", 18));
+
+		Map<Integer, Integer> intMap = new HashMap<Integer, Integer>();
+		intMap.put(1, 18);
+		intMap.put(2, 19);
+		intMap.put(3, 20);
+		System.out.println(intMap);
+		assertThat(intMap).containsOnly(entry(2, 19), entry(3, 20), entry(1, 18));
 	}
 
 	@Test
@@ -135,6 +137,13 @@ public class AssertJMapAssertionsTest {
 		map.put("Lemon", 20);
 		System.out.println(map);
 		assertThat(map).containsExactly(entry("Grape", 18), entry("Kiwifruit", 19), entry("Lemon", 20));
+
+		Map<Integer, Integer> intMap = new HashMap<Integer, Integer>();
+		intMap.put(1, 18);
+		intMap.put(2, 19);
+		intMap.put(3, 20);
+		System.out.println(intMap);
+		assertThat(intMap).containsExactly(entry(1, 18), entry(2, 19), entry(3, 20));
 	}
 
 	@Test
@@ -145,22 +154,12 @@ public class AssertJMapAssertionsTest {
 		map.put("Lemon", 20);
 		System.out.println(map);
 		assertThat(map).containsAnyOf(entry("Papaya", 28), entry("Grape", 18), entry("Strawberry", 38));
-	}
-
-	@Test
-	public void isEqualTo() {
-		Fruit grape = new Fruit("Grape", 18, 3);
-		Fruit kiwifruit = new Fruit("Kiwifruit", 19, 3);
-		Fruit lemon = new Fruit("Lemon", 20, 3);
-		Map<String, Fruit> map = new LinkedHashMap<String, Fruit>();
-		map.put(grape.getName(), grape);
-		map.put(kiwifruit.getName(), kiwifruit);
-		map.put(lemon.getName(), lemon);
-		Map<String, Fruit> map2 = new LinkedHashMap<String, Fruit>();
-		map2.put(grape.getName(), grape);
-		map2.put(kiwifruit.getName(), kiwifruit);
-		map2.put(lemon.getName(), lemon);
-		System.out.println(map);
-		assertThat(map).isEqualTo(map2);
+		
+		Map<Integer, Integer> intMap = new HashMap<Integer, Integer>();
+		intMap.put(1, 18);
+		intMap.put(2, 19);
+		intMap.put(3, 20);
+		System.out.println(intMap);
+		assertThat(intMap).containsAnyOf(entry(9, 28), entry(1, 18), entry(3, 20));
 	}
 }
