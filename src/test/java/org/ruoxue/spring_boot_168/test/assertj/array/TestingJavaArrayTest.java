6b14128a66d44dd43e698edcc5cd5b712432ba10
mixaceh <mixaceh@gmail.com>
2023-08-18 18:02:01 +0800

    update test

--- src/test/java/org/ruoxue/spring_boot_168/test/assertj/array/TestingJavaArrayTest.java
+++ src/test/java/org/ruoxue/spring_boot_168/test/assertj/array/TestingJavaArrayTest.java
@@ -1,10 +1,8 @@
 package org.ruoxue.spring_boot_168.test.assertj.array;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.Arrays;
-import java.util.List;
 
 import org.assertj.core.api.Condition;
 import org.junit.jupiter.api.Test;
@@ -37,36 +35,47 @@ public class TestingJavaArrayTest {
 		assertThat(intArray).isNot(intLength);
 	}
 
-//	@Test
-//	public void isIn() {
-//		String[] array = new String[] { "Durian", "Guava", "Pitaya" };
-//		List<String> list = Arrays.asList("Durian", "Guava", "Pitaya");
-//		System.out.println(Arrays.toString(array));
-//		System.out.println(list);
-//		assertThat(array).isIn(list);
-//
-//		int[] intArray = new int[] { 1, 2, 3, 4, 5 };
-//		List<Integer> intList = Arrays.asList(1, 2, 3, 4, 5);
-//		System.out.println(Arrays.toString(intArray));
-//		System.out.println(intList);
-//		assertThat(intArray).isIn(intList);
-//	}
-//
-//	@Test
-//	public void isInWithArray() {
-//		String[] array = new String[] { "Durian", "Guava", "Pitaya" };
-//		assertThat(array).isIn((Object[]) null);
-//	}
-//
-//	@Test
-//	public void isNotIn() {
-//		String[] array = new String[] { "Durian", "Guava", "Pitaya" };
-//		assertThat(array).isNotIn((Iterable) null);
-//	}
-//
-//	@Test
-//	public void isNotInWithArray() {
-//		String[] array = new String[] { "Durian", "Guava", "Pitaya" };
-//		assertThat(array).isNotIn((Object[]) null);
-//	}
+	@Test
+	public void isIn() {
+		String[] array = new String[] { "Durian", "Guava", "Pitaya" };
+		System.out.println(Arrays.toString(array));
+		assertThat("Guava").isIn(array);
+
+		Integer[] intArray = new Integer[] { 1, 2, 3, 4, 5 };
+		System.out.println(Arrays.toString(intArray));
+		assertThat(new Integer(3)).isIn(intArray);
+	}
+
+	@Test
+	public void isNotIn() {
+		String[] array = new String[] { "Durian", "Guava", "Pitaya" };
+		System.out.println(Arrays.toString(array));
+		assertThat("Mango").isNotIn(array);
+
+		Integer[] intArray = new Integer[] { 1, 2, 3, 4, 5 };
+		System.out.println(Arrays.toString(intArray));
+		assertThat(new Integer(9)).isNotIn(intArray);
+	}
+
+	@Test
+	public void startsWith() {
+		String[] array = new String[] { "Durian", "Guava", "Pitaya" };
+		System.out.println(Arrays.toString(array));
+		assertThat(array).startsWith("Durian");
+
+		Integer[] intArray = new Integer[] { 1, 2, 3, 4, 5 };
+		System.out.println(Arrays.toString(intArray));
+		assertThat(intArray).startsWith(1);
+	}
+	
+	@Test
+	public void endsWith() {
+		String[] array = new String[] { "Durian", "Guava", "Pitaya" };
+		System.out.println(Arrays.toString(array));
+		assertThat(array).endsWith("Pitaya");
+
+		Integer[] intArray = new Integer[] { 1, 2, 3, 4, 5 };
+		System.out.println(Arrays.toString(intArray));
+		assertThat(intArray).endsWith(5);
+	}
 }
