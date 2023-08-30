ece9fc2cd060cc48264766ef41d8449690a7a845
ruoxue <ruoxue777@gmail.com>
2023-08-30 11:31:47 +0800

    update test

--- src/test/java/org/ruoxue/spring_boot_168/test/assertj/number/AssertJNumberWithExamplesTest.java
+++ src/test/java/org/ruoxue/spring_boot_168/test/assertj/number/AssertJNumberWithExamplesTest.java
@@ -2,9 +2,6 @@ package org.ruoxue.spring_boot_168.test.assertj.number;
 
 import static org.assertj.core.api.Assertions.*;
 
-import java.util.Arrays;
-import java.util.List;
-
 import org.assertj.core.api.Condition;
 import org.junit.jupiter.api.Test;
 
@@ -14,82 +11,80 @@ public class AssertJNumberWithExamplesTest {
 	public void has() {
 		int value = 0;
 		System.out.println(value);
-		assertThat(value).has(null);
+		Condition<Integer> equals = new Condition<Integer>(i -> i == 0, "equals");
+		assertThat(value).has(equals);
 
 		double doubleValue = 0d;
 		System.out.println(doubleValue);
-		assertThat(doubleValue).has(null);
+		Condition<Double> doubleEquals = new Condition<Double>(i -> Double.compare(i, 0) == 0, "equals");
+		assertThat(doubleValue).has(doubleEquals);
 	}
 
 	@Test
 	public void hasToString() {
-		String[] array = new String[] { "Durian", "Guava", "Pitaya" };
-		System.out.println(Arrays.toString(array));
-		assertThat(array).hasToString(array.toString());
+		int value = 0;
+		System.out.println(value);
+		assertThat(value).hasToString(Integer.valueOf(value).toString());
 
-		int[] intArray = new int[] { 1, 2, 3, 4, 5 };
-		System.out.println(Arrays.toString(intArray));
-		assertThat(intArray).hasToString(intArray.toString());
+		double doubleValue = 0d;
+		System.out.println(doubleValue);
+		assertThat(doubleValue).hasToString(Double.valueOf(doubleValue).toString());
 	}
 
 	@Test
 	public void hasSameClassAs() {
-		String[] array = new String[] { "Durian", "Guava", "Pitaya" };
-		List<String> list = Arrays.asList("Durian", "Guava", "Pitaya");
-		System.out.println(Arrays.toString(array));
-		System.out.println(list);
-		assertThat(array).hasSameSizeAs(list);
+		int value = 0;
+		System.out.println(value);
+		assertThat(value).hasSameClassAs(0);
 
-		int[] intArray = new int[] { 1, 2, 3, 4, 5 };
-		List<Integer> intList = Arrays.asList(1, 2, 3, 4, 5);
-		System.out.println(Arrays.toString(intArray));
-		System.out.println(intList);
-		assertThat(intArray).hasSameSizeAs(intList);
+		double doubleValue = 0d;
+		System.out.println(doubleValue);
+		assertThat(doubleValue).hasSameClassAs(0d);
 	}
 
 	@Test
 	public void returns() {
 		int value = 0;
 		System.out.println(value);
-		assertThat(value).returns(null, null);
+		assertThat(value).returns(1, i -> new Integer(i + 1));
 
 		double doubleValue = 0d;
 		System.out.println(doubleValue);
-		assertThat(doubleValue).returns(null, null);
+		assertThat(doubleValue).returns(1d, i -> new Double(i + 1));
 	}
 
 	@Test
 	public void doesNotHave() {
-		String[] array = new String[] { "Durian", "Guava", "Pitaya" };
-		System.out.println(Arrays.toString(array));
-		Condition<String[]> length = new Condition<String[]>(s -> s.length > 3, "length");
-		assertThat(array).doesNotHave(length);
+		int value = 0;
+		System.out.println(value);
+		Condition<Integer> notEquals = new Condition<Integer>(i -> i != 0, "notEquals");
+		assertThat(value).doesNotHave(notEquals);
 
-		int[] intArray = new int[] { 1, 2, 3, 4, 5 };
-		System.out.println(Arrays.toString(intArray));
-		Condition<int[]> intLength = new Condition<int[]>(i -> i.length > 5, "length");
-		assertThat(intArray).doesNotHave(intLength);
+		double doubleValue = 0d;
+		System.out.println(doubleValue);
+		Condition<Double> doubleNotEquals = new Condition<Double>(i -> Double.compare(i, 0) != 0, "notEquals");
+		assertThat(doubleValue).doesNotHave(doubleNotEquals);
 	}
 
 	@Test
 	public void doesNotHaveSameClassAs() {
-		String[] array = new String[] { "Durian", "Guava", "Pitaya" };
-		System.out.println(Arrays.toString(array));
-		assertThat(array).doesNotHaveDuplicates();
+		int value = 0;
+		System.out.println(value);
+		assertThat(value).doesNotHaveSameClassAs(0d);
 
-		int[] intArray = new int[] { 1, 2, 3, 4, 5 };
-		System.out.println(Arrays.toString(intArray));
-		assertThat(intArray).doesNotHaveDuplicates();
+		double doubleValue = 0d;
+		System.out.println(doubleValue);
+		assertThat(doubleValue).doesNotHaveSameClassAs(0);
 	}
 
 	@Test
 	public void doesNotReturn() {
 		int value = 0;
 		System.out.println(value);
-		assertThat(value).doesNotReturn(null, null);
+		assertThat(value).doesNotReturn(2, i -> new Integer(i + 1));
 
 		double doubleValue = 0d;
 		System.out.println(doubleValue);
-		assertThat(doubleValue).doesNotReturn(null, null);
+		assertThat(doubleValue).doesNotReturn(2d, i -> new Double(i + 1));
 	}
 }
