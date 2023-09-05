2bb1cad8407bcd7af71b881d313bf8213522bd7d
ruoxue <ruoxue777@gmail.com>
2023-09-05 21:12:35 +0800

    update test

--- src/test/java/org/ruoxue/spring_boot_168/test/assertj/localdatetime/UnitTestAssertJLocalDateTimeWithExamplesTest.java
+++ src/test/java/org/ruoxue/spring_boot_168/test/assertj/localdatetime/UnitTestAssertJLocalDateTimeWithExamplesTest.java
@@ -11,44 +11,60 @@ public class UnitTestAssertJLocalDateTimeWithExamplesTest {
 	@Test
 	public void isEqualToIgnoringHours() {
 		LocalDateTime value = LocalDateTime.of(2023, 10, 31, 5, 6, 7);
+		LocalDateTime value2 = LocalDateTime.of(2023, 10, 31, 0, 6, 7);
 		System.out.println(value);
-		assertThat(value).isEqualToIgnoringHours(LocalDateTime.of(2023, 10, 31, 0, 6, 7));
+		System.out.println(value2);
+		assertThat(value).isEqualToIgnoringHours(value2);
 
 		value = LocalDateTime.parse("2000-01-01T00:00:00");
+		value2 = LocalDateTime.parse("2000-01-01T01:00:00");
 		System.out.println(value);
-		assertThat(value).isEqualToIgnoringHours(LocalDateTime.parse("2000-01-01T01:00:00"));
+		System.out.println(value2);
+		assertThat(value).isEqualToIgnoringHours(value2);
 	}
 
 	@Test
 	public void isEqualToIgnoringMinutes() {
 		LocalDateTime value = LocalDateTime.of(2023, 10, 31, 5, 6, 7);
+		LocalDateTime value2 = LocalDateTime.of(2023, 10, 31, 5, 0, 7);
 		System.out.println(value);
-		assertThat(value).isEqualToIgnoringMinutes(LocalDateTime.of(2023, 10, 31, 5, 0, 7));
+		System.out.println(value2);
+		assertThat(value).isEqualToIgnoringHours(value2);
 
 		value = LocalDateTime.parse("2000-01-01T00:00:00");
+		value2 = LocalDateTime.parse("2000-01-01T00:01:00");
 		System.out.println(value);
-		assertThat(value).isEqualToIgnoringMinutes(LocalDateTime.parse("2000-01-01T00:01:00"));
+		System.out.println(value2);
+		assertThat(value).isEqualToIgnoringHours(value2);
 	}
 
 	@Test
 	public void isEqualToIgnoringSeconds() {
 		LocalDateTime value = LocalDateTime.of(2023, 10, 31, 5, 6, 7);
+		LocalDateTime value2 = LocalDateTime.of(2023, 10, 31, 5, 6, 0);
 		System.out.println(value);
-		assertThat(value).isEqualToIgnoringSeconds(LocalDateTime.of(2023, 10, 31, 5, 6, 0));
+		System.out.println(value2);
+		assertThat(value).isEqualToIgnoringHours(value2);
 
 		value = LocalDateTime.parse("2000-01-01T00:00:00");
+		value2 = LocalDateTime.parse("2000-01-01T00:00:01");
 		System.out.println(value);
-		assertThat(value).isEqualToIgnoringSeconds(LocalDateTime.parse("2000-01-01T00:00:01"));
+		System.out.println(value2);
+		assertThat(value).isEqualToIgnoringHours(value2);
 	}
 
 	@Test
 	public void isEqualToIgnoringNanos() {
-		LocalDateTime value = LocalDateTime.of(2023, 10, 31, 5, 6, 7, 123);
+		LocalDateTime value = LocalDateTime.of(2023, 10, 31, 5, 6, 7);
+		LocalDateTime value2 = LocalDateTime.of(2023, 10, 31, 5, 6, 7, 123);
 		System.out.println(value);
-		assertThat(value).isEqualToIgnoringNanos(LocalDateTime.of(2023, 10, 31, 5, 6, 7));
+		System.out.println(value2);
+		assertThat(value).isEqualToIgnoringHours(value2);
 
-		value = LocalDateTime.parse("2000-01-01T00:00:00.123");
+		value = LocalDateTime.parse("2000-01-01T00:00:00");
+		value2 = LocalDateTime.parse("2000-01-01T00:00:00.123");
 		System.out.println(value);
-		assertThat(value).isEqualToIgnoringNanos(LocalDateTime.parse("2000-01-01T00:00"));
+		System.out.println(value2);
+		assertThat(value).isEqualToIgnoringHours(value2);
 	}
 }
