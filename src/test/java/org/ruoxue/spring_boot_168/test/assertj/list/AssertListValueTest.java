6562b0489abad28dbe082f9a279e73c72f36e0db
ruoxue <ruoxue777@gmail.com>
2023-09-12 20:08:40 +0800

    update

--- src/test/java/org/ruoxue/spring_boot_168/test/assertj/list/AssertListValueTest.java
+++ src/test/java/org/ruoxue/spring_boot_168/test/assertj/list/AssertListValueTest.java
@@ -3,14 +3,11 @@ package org.ruoxue.spring_boot_168.test.assertj.list;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
-import java.util.function.Predicate;
 
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
-import org.assertj.core.api.Condition;
 import org.junit.jupiter.api.Test;
 
 import lombok.Getter;
@@ -65,156 +62,77 @@ public class AssertListValueTest {
 
 	@Test
 	public void matches() {
-		Fruit grape = new Fruit("Grape", Double.MAX_VALUE, 1);
-		Fruit kiwifruit = new Fruit("Kiwifruit", 1, 2);
-		Fruit lemon = new Fruit("Lemon", -1, 3);
-		Map<String, Fruit> map = new HashMap<>();
-		map.put(grape.getName(), grape);
-		map.put(kiwifruit.getName(), kiwifruit);
-		map.put(lemon.getName(), lemon);
-		System.out.println(map);
-		assertThat(map).matches(m -> m.get("Kiwifruit") != null);
-
-		Predicate<Map<String, Fruit>> keyQuantity = m -> m.entrySet().stream()
-				.anyMatch((Map.Entry<String, Fruit> e) -> "Kiwifruit".equals(e.getKey())
-						&& Double.compare(e.getValue().getQuantity(), 1d) == 0);
-		assertThat(map).matches(keyQuantity, "keyQuantity");
-	}
-
-	@Test
-	public void satisfies() {
-		Fruit grape = new Fruit("Grape", Double.MAX_VALUE, 1);
-		Fruit kiwifruit = new Fruit("Kiwifruit", 1, 2);
-		Fruit lemon = new Fruit("Lemon", -1, 3);
-		Map<String, Fruit> map = new HashMap<>();
-		map.put(grape.getName(), grape);
-		map.put(kiwifruit.getName(), kiwifruit);
-		map.put(lemon.getName(), lemon);
-		System.out.println(map);
-		Condition<Map<String, Fruit>> size = new Condition<Map<String, Fruit>>(m -> m.size() > 2, "size");
-		assertThat(map).satisfies(size);
-
-		Predicate<Map<String, Fruit>> predcate = m -> m.entrySet().stream()
-				.anyMatch((Map.Entry<String, Fruit> e) -> "Kiwifruit".equals(e.getKey())
-						&& Double.doubleToLongBits(e.getValue().getQuantity()) == Double.doubleToLongBits(1d));
-		Condition<Map<String, Fruit>> keyQuantity = new Condition<Map<String, Fruit>>(predcate, "keyQuantity");
-		assertThat(map).satisfies(keyQuantity);
+		Fruit apple = new Fruit("Apple", Double.MAX_VALUE, 1);
+		Fruit banana = new Fruit("Banana", 1, 2);
+		Fruit cherry = new Fruit("Cherry", -1, 3);
+		List<Fruit> list = Arrays.asList(apple, banana, cherry);
+		System.out.println(list);
+		assertThat(list).matches(l -> l.size() > 2);
 	}
 
 	@Test
 	public void satisfiesAnyOf() {
-		Fruit grape = new Fruit("Grape", Double.MAX_VALUE, 1);
-		Fruit kiwifruit = new Fruit("Kiwifruit", 1, 2);
-		Fruit lemon = new Fruit("Lemon", -1, 3);
-		Map<String, Fruit> map = new HashMap<>();
-		map.put(grape.getName(), grape);
-		map.put(kiwifruit.getName(), kiwifruit);
-		map.put(lemon.getName(), lemon);
-		System.out.println(map);
-		assertThat(map).satisfiesAnyOf(m -> {
-			assertThat(m.get("Kiwifruit")).isNotNull();
+		Fruit apple = new Fruit("Apple", Double.MAX_VALUE, 1);
+		Fruit banana = new Fruit("Banana", 1, 2);
+		Fruit cherry = new Fruit("Cherry", -1, 3);
+		List<Fruit> list = Arrays.asList(apple, banana, cherry);
+		System.out.println(list);
+		assertThat(list).satisfiesAnyOf(l -> {
+			assertThat(l.get(1)).isNotNull();
 		});
 	}
 
 	@Test
-	public void hasFieldOrProperty() {
-		Fruit grape = new Fruit("Grape", Double.MAX_VALUE, 1);
-		Fruit kiwifruit = new Fruit("Kiwifruit", 1, 2);
-		Fruit lemon = new Fruit("Lemon", -1, 3);
-		Map<String, Fruit> map = new HashMap<>();
-		map.put(grape.getName(), grape);
-		map.put(kiwifruit.getName(), kiwifruit);
-		map.put(lemon.getName(), lemon);
-		System.out.println(map);
-		assertThat(map).hasFieldOrProperty("Kiwifruit");
-
-	}
-
-	@Test
-	public void hasFieldOrPropertyWithValue() {
-		Fruit grape = new Fruit("Grape", Double.MAX_VALUE, 1);
-		Fruit kiwifruit = new Fruit("Kiwifruit", 1, 2);
-		Fruit lemon = new Fruit("Lemon", -1, 3);
-		Map<String, Fruit> map = new HashMap<>();
-		map.put(grape.getName(), grape);
-		map.put(kiwifruit.getName(), kiwifruit);
-		map.put(lemon.getName(), lemon);
-		System.out.println(map);
-		assertThat(map).hasFieldOrPropertyWithValue("Kiwifruit", kiwifruit);
-	}
-
-	@Test
 	public void hasAllNullFieldsOrProperties() {
-		SimpleFruit grape = new SimpleFruit("Grape");
-		grape.setName(null);
-		grape.setOrigins(null);
-		SimpleFruit kiwifruit = new SimpleFruit("Kiwifruit");
-		kiwifruit.setName(null);
-		kiwifruit.setOrigins(null);
-		SimpleFruit lemon = new SimpleFruit("Lemon");
-		lemon.setName(null);
-		lemon.setOrigins(null);
-		Map<String, SimpleFruit> map = new HashMap<>();
-		map.put("Grape", grape);
-		map.put("Kiwifruit", kiwifruit);
-		map.put("Lemon", lemon);
-		System.out.println(map);
-		assertThat(map).allSatisfy((k, v) -> {
-			assertThat(v).hasAllNullFieldsOrProperties();
+		SimpleFruit apple = new SimpleFruit();
+		SimpleFruit banana = new SimpleFruit();
+		SimpleFruit cherry = new SimpleFruit();
+		List<SimpleFruit> list = Arrays.asList(apple, banana, cherry);
+		System.out.println(list);
+		assertThat(list).allSatisfy(e -> {
+			assertThat(e).hasAllNullFieldsOrProperties();
 		});
 	}
 
 	@Test
 	public void hasAllNullFieldsOrPropertiesExcept() {
-		Fruit grape = new Fruit("Grape", Double.MAX_VALUE, 1);
-		grape.setName(null);
-		grape.setOrigins(null);
-		Fruit kiwifruit = new Fruit("Kiwifruit", 1, 2);
-		kiwifruit.setName(null);
-		kiwifruit.setOrigins(null);
-		Fruit lemon = new Fruit("Lemon", -1, 3);
-		lemon.setName(null);
-		lemon.setOrigins(null);
-		Map<String, Fruit> map = new HashMap<>();
-		map.put("Grape", grape);
-		map.put("Kiwifruit", kiwifruit);
-		map.put("Lemon", lemon);
-		System.out.println(map);
-		assertThat(map).allSatisfy((k, v) -> {
-			assertThat(v).hasAllNullFieldsOrPropertiesExcept("type", "quantity");
+		Fruit apple = new Fruit("Apple", Double.MAX_VALUE, 1);
+		apple.setOrigins(null);
+		Fruit banana = new Fruit("Banana", 1, 2);
+		banana.setOrigins(null);
+		Fruit cherry = new Fruit("Cherry", -1, 3);
+		cherry.setOrigins(null);
+		List<Fruit> list = Arrays.asList(apple, banana, cherry);
+		System.out.println(list);
+		assertThat(list).allSatisfy(e -> {
+			assertThat(e).hasAllNullFieldsOrPropertiesExcept("name", "type", "quantity");
 		});
 	}
 
 	@Test
 	public void hasNoNullFieldsOrProperties() {
-		Fruit grape = new Fruit("Grape", Double.MAX_VALUE, 1);
-		Fruit kiwifruit = new Fruit("Kiwifruit", 1, 2);
-		Fruit lemon = new Fruit("Lemon", -1, 3);
-		Map<String, Fruit> map = new HashMap<>();
-		map.put(grape.getName(), grape);
-		map.put(kiwifruit.getName(), kiwifruit);
-		map.put(lemon.getName(), lemon);
-		System.out.println(map);
-		assertThat(map).allSatisfy((k, v) -> {
-			assertThat(v).hasNoNullFieldsOrProperties();
+		Fruit apple = new Fruit("Apple", Double.MAX_VALUE, 1);
+		Fruit banana = new Fruit("Banana", 1, 2);
+		Fruit cherry = new Fruit("Cherry", -1, 3);
+		List<Fruit> list = Arrays.asList(apple, banana, cherry);
+		System.out.println(list);
+		assertThat(list).allSatisfy(e -> {
+			assertThat(e).hasNoNullFieldsOrProperties();
 		});
 	}
 
 	@Test
 	public void hasNoNullFieldsOrPropertiesExcept() {
-		Fruit grape = new Fruit("Grape", Double.MAX_VALUE, 1);
-		grape.setOrigins(null);
-		Fruit kiwifruit = new Fruit("Kiwifruit", 1, 2);
-		kiwifruit.setOrigins(null);
-		Fruit lemon = new Fruit("Lemon", -1, 3);
-		lemon.setOrigins(null);
-		Map<String, Fruit> map = new HashMap<>();
-		map.put(grape.getName(), grape);
-		map.put(kiwifruit.getName(), kiwifruit);
-		map.put(lemon.getName(), lemon);
-		System.out.println(map);
-		assertThat(map).allSatisfy((k, v) -> {
-			assertThat(v).hasNoNullFieldsOrPropertiesExcept("origins");
+		Fruit apple = new Fruit("Apple", Double.MAX_VALUE, 1);
+		apple.setOrigins(null);
+		Fruit banana = new Fruit("Banana", 1, 2);
+		banana.setOrigins(null);
+		Fruit cherry = new Fruit("Cherry", -1, 3);
+		cherry.setOrigins(null);
+		List<Fruit> list = Arrays.asList(apple, banana, cherry);
+		System.out.println(list);
+		assertThat(list).allSatisfy(e -> {
+			assertThat(e).hasNoNullFieldsOrPropertiesExcept("origins");
 		});
 	}
 }
