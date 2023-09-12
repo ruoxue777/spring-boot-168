d726331491e70a3145b36985c261fbfcee7f4ba2
ruoxue <ruoxue777@gmail.com>
2023-09-12 11:20:57 +0800

    update readme

---
+++ src/test/java/org/ruoxue/spring_boot_168/test/assertj/exception/AssertJExceptionMethodsTest.java
@@ -0,0 +1,75 @@
+package org.ruoxue.spring_boot_168.test.assertj.exception;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+public class AssertJExceptionMethodsTest {
+
+	public int divide(int value1, int value2) {
+		return value1 / value2;
+	}
+
+	@Test
+	public void isInstanceOf() {
+		assertThatThrownBy(() -> divide(1, 0)).isInstanceOf(ArithmeticException.class).hasMessageContaining("zero")
+				.hasMessage("/ by zero");
+	}
+
+	@Test
+	public void hasMessageContaining() {
+		assertThatThrownBy(() -> {
+			List<Integer> list = Arrays.asList(1, 2);
+			list.get(2);
+		}).isInstanceOf(IndexOutOfBoundsException.class).hasMessageContaining("2");
+	}
+
+	@Test
+	public void hasCauseInstanceOf() {
+		assertThatThrownBy(() -> {
+			try {
+				throw new IOException();
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+		}).isInstanceOf(RuntimeException.class).hasCauseInstanceOf(IOException.class)
+				.hasStackTraceContaining("IOException");
+	}
+
+	@Test
+	public void isThrownBy() {
+		assertThatExceptionOfType(ArithmeticException.class).isThrownBy(() -> divide(1, 0))
+				.withMessageContaining("zero").withMessage("/ by zero");
+	}
+
+	@Test
+	public void withMessageContaining() {
+		assertThatExceptionOfType(IndexOutOfBoundsException.class).isThrownBy(() -> {
+			List<Integer> list = Arrays.asList(1, 2);
+			list.get(2);
+		}).withMessageContaining("2");
+	}
+
+	@Test
+	public void withCauseExactlyInstanceOf() {
+		assertThatExceptionOfType(RuntimeException.class).isThrownBy(() -> {
+			try {
+				throw new IOException();
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+		}).withCauseExactlyInstanceOf(IOException.class).withStackTraceContaining("IOException");
+	}
+
+	@Test
+	public void thrown() {
+		Throwable thrown = catchThrowable(() -> {
+			List<Integer> list = Arrays.asList(1, 2);
+			list.get(2);
+		});
+		assertThat(thrown).isInstanceOf(IndexOutOfBoundsException.class).hasMessageContaining("2");
+	}
+}
--- src/test/java/org/ruoxue/spring_boot_168/test/assertj/exception/README.md
+++ src/test/java/org/ruoxue/spring_boot_168/test/assertj/exception/README.md
@@ -2,3 +2,5 @@
 
 ## Related Articles
 1. [AssertJ Exception Assertions](https://www.ruoxue.org/assertj-155-assertj-exception-assertions/)
+2. [AssertJ Exception Methods](https://www.ruoxue.org/assertj-155-assertj-exception-methods/)
+3. [AssertJ Exception in Java with Examples](https://www.ruoxue.org/assertj-155-assertj-exception-in-java-with-examples/)
