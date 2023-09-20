d597cb6142fcde7314c1b76a41ba512ca8d4dfe8
ruoxue <ruoxue777@gmail.com>
2023-09-20 17:55:33 +0800

    update

--- src/test/java/org/ruoxue/spring_boot_168/test/assertj/object/AssertJObjectAssertionsTest.java
+++ src/test/java/org/ruoxue/spring_boot_168/test/assertj/object/AssertJObjectAssertionsTest.java
@@ -2,15 +2,50 @@ package org.ruoxue.spring_boot_168.test.assertj.object;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
 import org.junit.jupiter.api.Test;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 public class AssertJObjectAssertionsTest {
 
+	@NoArgsConstructor
+	@Getter
+	@Setter
+	public static class Fruit {
+		private String name;
+		private double quantity;
+		private int type;
+		private List<String> origins = new ArrayList<>();
+
+		public Fruit(String name, double quantity, int type) {
+			this.name = name;
+			this.quantity = quantity;
+			this.type = type;
+		}
+
+		public String toString() {
+			ToStringBuilder builder = new ToStringBuilder(this, ToStringStyle.JSON_STYLE);
+			builder.appendSuper(super.toString());
+			builder.append("name", name);
+			builder.append("quantity", quantity);
+			builder.append("type", type);
+			builder.append("origins", origins);
+			return builder.toString();
+		}
+	}
+	
 	@Test
 	public void contains() {
-		Object value = "AssertJ 155";
+		Object value = new Fruit("Litchi", Double.MAX_VALUE, 1);
 		System.out.println(value);
-		assertThat(value).contains("AssertJ", "155");
+		assertThat(value).matches(null, null);
 		assertThat(value).contains("rtJ", "5");
 	}
 
--- src/test/java/org/ruoxue/spring_boot_168/test/assertj/object/AssertJObjectMethodsTest.java
+++ src/test/java/org/ruoxue/spring_boot_168/test/assertj/object/AssertJObjectMethodsTest.java
@@ -2,6 +2,7 @@ package org.ruoxue.spring_boot_168.test.assertj.object;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.math.BigDecimal;
 import org.junit.jupiter.api.Test;
 
 public class AssertJObjectMethodsTest {
@@ -19,78 +20,46 @@ public class AssertJObjectMethodsTest {
 
 	@Test
 	public void isNotNull() {
-		Object value = "AssertJ 155";
+		Object value = "AssertJ";
 		System.out.println(value);
 		assertThat(value).isNotNull();
 
-		value = "AssertJ";
+		value = BigDecimal.valueOf(155);
 		System.out.println(value);
 		assertThat(value).isNotNull();
-	}
-
-	@Test
-	public void isEmpty() {
-		Object value = "";
-		System.out.println(value);
-		assertThat(value).isEmpty();
-
-		Object value2 = "";
-		System.out.println(value2);
-		assertThat(value2).isEmpty();
-	}
-
-	@Test
-	public void isNotEmpty() {
-		Object value = "AssertJ 155";
-		System.out.println(value);
-		assertThat(value).isNotEmpty();
-
-		value = "AssertJ";
-		System.out.println(value);
-		assertThat(value).isNotEmpty();
-	}
-
-	@Test
-	public void isNullOrEmpty() {
-		Object value = null;
-		System.out.println(value);
-		assertThat(value).isNullOrEmpty();
-		value = "";
-		System.out.println(value);
-		assertThat(value).isNullOrEmpty();
 	}
 
 	@Test
 	public void isEqualTo() {
-		Object value = "AssertJ 155";
-		System.out.println(value);
-		assertThat(value).isEqualTo("AssertJ 155");
-
-		value = "AssertJ";
+		Object value = "AssertJ";
 		System.out.println(value);
 		assertThat(value).isEqualTo("AssertJ");
+
+		value = BigDecimal.valueOf(155);
+		System.out.println(value);
+		assertThat(value).isEqualTo(BigDecimal.valueOf(155));
 	}
 
 	@Test
 	public void isNotEqualTo() {
-		Object value = "AssertJ 155";
+		Object value = "AssertJ";
 		System.out.println(value);
-		assertThat(value).isNotEqualTo("AssertJ");
+		assertThat(value).isNotEqualTo("155");
 
-		value = "AssertJ";
+		value = BigDecimal.valueOf(155);
 		System.out.println(value);
-		assertThat(value).isNotEqualTo("JUnit");
+		assertThat(value).isNotEqualTo(155);
 	}
 
 	@Test
 	public void isSameAs() {
-		Object value = "AssertJ 155";
+		Object value = "AssertJ";
 		Object value2 = value;
 		System.out.println(value);
 		System.out.println(value2);
 		assertThat(value).isSameAs(value2);
 
-		value = "AssertJ";
+		value = BigDecimal.valueOf(155);
 		value2 = value;
 		System.out.println(value);
 		System.out.println(value2);
@@ -99,14 +68,14 @@ public class AssertJObjectMethodsTest {
 
 	@Test
 	public void isNotSameAs() {
-		Object value = "AssertJ 155";
-		Object value2 = new Object("AssertJ 155");
+		Object value = "AssertJ";
+		Object value2 = new String("AssertJ");
 		System.out.println(value);
 		System.out.println(value2);
 		assertThat(value).isNotSameAs(value2);
 
-		value = "AssertJ";
-		value2 = new Object("AssertJ");
+		value = BigDecimal.valueOf(155);
+		value2 = BigDecimal.valueOf(155);
 		System.out.println(value);
 		System.out.println(value2);
 		assertThat(value).isNotSameAs(value2);
