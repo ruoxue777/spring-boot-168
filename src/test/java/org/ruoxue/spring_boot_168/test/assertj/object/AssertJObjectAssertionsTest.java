3ab0c1c02a295c4c2ac25875f31f0a06b6ae477a
ruoxue <ruoxue777@gmail.com>
2023-09-21 19:21:58 +0800

    update contains

--- src/test/java/org/ruoxue/spring_boot_168/test/assertj/object/AssertJObjectAssertionsTest.java
+++ src/test/java/org/ruoxue/spring_boot_168/test/assertj/object/AssertJObjectAssertionsTest.java
@@ -43,74 +43,75 @@ public class AssertJObjectAssertionsTest {
 
 	@Test
 	public void contains() {
-		Object value = new Fruit("Litchi", Double.MAX_VALUE, 1);
+		Object value = new Fruit("Litchi 101", Double.MAX_VALUE, 1);
 		System.out.println(value);
-		assertThat(value).extracting("name").asInstanceOf(STRING).contains("Litchi");
+		assertThat(value).extracting("name").asInstanceOf(STRING).contains("Litchi", "101");
+		assertThat(value).extracting("name").asInstanceOf(STRING).contains("chi", "1");
 	}
 
-//	@Test
-//	public void doesNotContain() {
-//		Object value = "AssertJ 155";
-//		System.out.println(value);
-//		assertThat(value).doesNotContain("JUnit", "151");
-//		assertThat(value).doesNotContain("nit", "51");
-//	}
-//
-//	@Test
-//	public void containsOnlyOnce() {
-//		Object value = "AssertJ 155";
-//		System.out.println(value);
-//		assertThat(value).containsOnlyOnce("rtJ");
-//		assertThat(value).containsOnlyOnce("55");
-//	}
-//
-//	@Test
-//	public void containsOnlyWhitespaces() {
-//		Object value = " ";
-//		System.out.println(value);
-//		assertThat(value).containsOnlyWhitespaces();
-//		value = "   ";
-//		System.out.println(value);
-//		assertThat(value).containsOnlyWhitespaces();
-//	}
-//
-//	@Test
-//	public void doesNotContainOnlyWhitespaces() {
-//		Object value = "AssertJ 155";
-//		System.out.println(value);
-//		assertThat(value).doesNotContainOnlyWhitespaces();
-//		value = "AssertJ ";
-//		System.out.println(value);
-//		assertThat(value).doesNotContainOnlyWhitespaces();
-//	}
-//
-//	@Test
-//	public void containsOnlyDigits() {
-//		Object value = "155";
-//		System.out.println(value);
-//		assertThat(value).containsOnlyDigits();
-//		value = "5";
-//		System.out.println(value);
-//		assertThat(value).containsOnlyDigits();
-//	}
-//
-//	@Test
-//	public void containsWhitespaces() {
-//		Object value = "AssertJ 155";
-//		System.out.println(value);
-//		assertThat(value).containsWhitespaces();
-//		value = " ";
-//		System.out.println(value);
-//		assertThat(value).containsWhitespaces();
-//	}
-//
-//	@Test
-//	public void doesNotContainAnyWhitespaces() {
-//		Object value = "AssertJ155";
-//		System.out.println(value);
-//		assertThat(value).doesNotContainAnyWhitespaces();
-//		value = "AssertJ";
-//		System.out.println(value);
-//		assertThat(value).doesNotContainAnyWhitespaces();
-//	}
+	@Test
+	public void doesNotContain() {
+		Object value = new Fruit("Litchi 101", Double.MAX_VALUE, 1);
+		System.out.println(value);
+		assertThat(value).extracting("name").asInstanceOf(STRING).doesNotContain("Blueberry", "99");
+		assertThat(value).extracting("name").asInstanceOf(STRING).doesNotContain("rry", "9");
+	}
+
+	@Test
+	public void containsOnlyOnce() {
+		Object value = new Fruit("Litchi 101", Double.MAX_VALUE, 1);
+		System.out.println(value);
+		assertThat(value).extracting("name").asInstanceOf(STRING).containsOnlyOnce("chi");
+		assertThat(value).extracting("name").asInstanceOf(STRING).containsOnlyOnce("01");
+	}
+
+	@Test
+	public void containsOnlyWhitespaces() {
+		Object value = new Fruit(" ", Double.MAX_VALUE, 1);
+		System.out.println(value);
+		assertThat(value).extracting("name").asInstanceOf(STRING).containsOnlyWhitespaces();
+		value = new Fruit("   ", Double.MAX_VALUE, 1);
+		System.out.println(value);
+		assertThat(value).extracting("name").asInstanceOf(STRING).containsOnlyWhitespaces();
+	}
+
+	@Test
+	public void doesNotContainOnlyWhitespaces() {
+		Object value = new Fruit("Litchi 101", Double.MAX_VALUE, 1);
+		System.out.println(value);
+		assertThat(value).extracting("name").asInstanceOf(STRING).doesNotContainOnlyWhitespaces();
+		value = new Fruit("Litchi ", Double.MAX_VALUE, 1);
+		System.out.println(value);
+		assertThat(value).extracting("name").asInstanceOf(STRING).doesNotContainOnlyWhitespaces();
+	}
+
+	@Test
+	public void containsOnlyDigits() {
+		Object value = new Fruit("101", Double.MAX_VALUE, 1);
+		System.out.println(value);
+		assertThat(value).extracting("name").asInstanceOf(STRING).containsOnlyDigits();
+		value = new Fruit("1", Double.MAX_VALUE, 1);
+		System.out.println(value);
+		assertThat(value).extracting("name").asInstanceOf(STRING).containsOnlyDigits();
+	}
+
+	@Test
+	public void containsWhitespaces() {
+		Object value = new Fruit("Litchi 101", Double.MAX_VALUE, 1);
+		System.out.println(value);
+		assertThat(value).extracting("name").asInstanceOf(STRING).containsWhitespaces();
+		value = new Fruit(" ", Double.MAX_VALUE, 1);
+		System.out.println(value);
+		assertThat(value).extracting("name").asInstanceOf(STRING).containsWhitespaces();
+	}
+
+	@Test
+	public void doesNotContainAnyWhitespaces() {
+		Object value = new Fruit("Litchi101", Double.MAX_VALUE, 1);
+		System.out.println(value);
+		assertThat(value).extracting("name").asInstanceOf(STRING).doesNotContainAnyWhitespaces();
+		value = new Fruit("Litchi", Double.MAX_VALUE, 1);
+		System.out.println(value);
+		assertThat(value).extracting("name").asInstanceOf(STRING).doesNotContainAnyWhitespaces();
+	}
 }
