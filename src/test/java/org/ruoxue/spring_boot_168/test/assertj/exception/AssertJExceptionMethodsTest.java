248378ec3e2062ff71c509d556f6fbe095fa3953
ruoxue <ruoxue777@gmail.com>
2023-09-23 23:34:18 +0800

    update test

--- src/test/java/org/ruoxue/spring_boot_168/test/assertj/exception/AssertJExceptionMethodsTest.java
+++ src/test/java/org/ruoxue/spring_boot_168/test/assertj/exception/AssertJExceptionMethodsTest.java
@@ -27,49 +27,56 @@ public class AssertJExceptionMethodsTest {
 	@Test
 	public void exception() {
 		assertThatException().isThrownBy(() -> {
-			throw new Exception("ex");
-		}).withMessage("ex").withStackTraceContaining("Exception");
+			throw new Exception("ex !");
+		}).withMessage("ex !").withMessage("%s !", "ex").withMessageContaining("ex")
+				.withStackTraceContaining("Exception").withNoCause();
 	}
 
 	@Test
 	public void illegalArgumentException() {
 		assertThatIllegalArgumentException().isThrownBy(() -> {
-			throw new IllegalArgumentException("illegal");
-		}).withMessage("illegal").withStackTraceContaining("IllegalArgumentException");
+			throw new IllegalArgumentException("illegal !");
+		}).withMessage("illegal !").withMessage("%s !", "illegal").withMessageContaining("illegal")
+				.withStackTraceContaining("IllegalArgumentException").withNoCause();
 	}
 
 	@Test
 	public void illegalStateException() {
 		assertThatIllegalStateException().isThrownBy(() -> {
-			throw new IllegalStateException("illegal");
-		}).withMessage("illegal").withStackTraceContaining("IllegalStateException");
+			throw new IllegalStateException("illegal !");
+		}).withMessage("illegal !").withMessage("%s !", "illegal").withMessageContaining("illegal")
+				.withStackTraceContaining("IllegalStateException").withNoCause();
 	}
 
 	@Test
 	public void indexOutOfBoundsException() {
 		assertThatIndexOutOfBoundsException().isThrownBy(() -> {
-			throw new IndexOutOfBoundsException("index");
-		}).withMessage("index").withStackTraceContaining("IndexOutOfBoundsException");
+			throw new IndexOutOfBoundsException("index !");
+		}).withMessage("index !").withMessage("%s !", "index").withMessageContaining("index")
+				.withStackTraceContaining("IndexOutOfBoundsException").withNoCause();
 	}
 
 	@Test
 	public void ioException() {
 		assertThatIOException().isThrownBy(() -> {
-			throw new IOException("io");
-		}).withMessage("io").withStackTraceContaining("IOException");
+			throw new IOException("io !");
+		}).withMessage("io !").withMessage("%s !", "io").withMessageContaining("io")
+				.withStackTraceContaining("IOException").withNoCause();
 	}
 
 	@Test
 	public void nullPointerException() {
 		assertThatNullPointerException().isThrownBy(() -> {
-			throw new NullPointerException("null");
-		}).withMessage("null").withStackTraceContaining("NullPointerException");
+			throw new NullPointerException("null !");
+		}).withMessage("null !").withMessage("%s !", "null").withMessageContaining("null")
+				.withStackTraceContaining("NullPointerException").withNoCause();
 	}
 
 	@Test
 	public void runtimeException() {
 		assertThatRuntimeException().isThrownBy(() -> {
-			throw new RuntimeException("run");
-		}).withMessage("run").withStackTraceContaining("RuntimeException");
+			throw new RuntimeException("run !");
+		}).withMessage("run !").withMessage("%s !", "run").withMessageContaining("run")
+				.withStackTraceContaining("RuntimeException").withNoCause();
 	}
 }
